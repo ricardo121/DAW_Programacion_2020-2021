dd44fd1c135fbef9fb7d3ba4b5701b438463f7f7
ricardo121 <ricardoasimbaya@gmail.com>
2020-12-14 16:43:16 +0100

    Add files via upload

---
+++ U3/Chimborazo_Asimbaya_U3_Examen_B/Ejercicio1.java
@@ -0,0 +1,175 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Ejercicio1 {
+
+
+
+        public static int subcorteza(int[][] n, int posicion){
+
+            //Hallar el numero de para decir el numero de valores para el nuevo array
+            int tamai= n.length;
+            int tamaj= n[1].length;
+            int resultado = 0;
+            //System.out.println("tabla   "+ tamai);
+            //System.out.println("tabla  adio "+ tamaj);
+
+            int filas = 0;
+
+
+            int cont1 = 0;
+            int cont2 = 0;
+
+
+            for (int j = 0; j < tamaj; j++){
+
+                cont1 = cont1 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+
+                cont1 = cont1 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+
+                cont1 = cont1 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+
+                cont1 = cont1 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+
+                cont1 = cont1 + 1;
+
+            }
+
+
+            int resu[] = new int[cont1];
+
+            for (int j = 0; j < tamaj; j++){
+
+                resu[cont2] = n[filas][j];
+                cont2 = cont2 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+                resu[cont2] = n[filas+1][j];
+                cont2 = cont2 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+                resu[cont2] = n[filas+2][j];
+                cont2 = cont2 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+                resu[cont2] = n[filas+3][j];
+                cont2 = cont2 + 1;
+
+            }
+            for (int j = 0; j < tamaj; j++){
+
+                resu[cont2] = n[filas+4][j];
+                cont2 = cont2 + 1;
+
+            }
+
+            ///
+            if (posicion<cont1) {
+                for (int i = 0; i < tamai; i++) {
+
+
+                    resultado = resu[posicion];
+
+
+                }
+
+
+                int longitud = resu.length;
+                for (int i = 0; i < longitud; i++) {
+                    if (posicion  == i) {
+                        resultado = resu[i];
+                    }
+                }
+            }else{
+                resultado=-1;
+            }
+
+            //System.out.println(Arrays.toString(resu));
+            //Devolver el Array Resultado[]
+            return resultado;
+
+
+        }
+
+
+        public static void main(String[] args) {
+
+            Scanner teclado = new Scanner (System.in);
+
+
+            int tamai = 5;
+            int tamaj = 6;
+            int tama = 6;
+
+            int n[][] = new int[tamai][tamaj];
+            int m[] = new int[tama];
+
+            for (int i = 0; i < tamai; i++){
+
+                for (int j = 0 ; j < tamaj ; j++){
+
+                    n[i][j] =(int) (Math.random() *(500-200+1) + 200);
+
+                }
+
+            }
+
+            for (int i = 0; i < tamai; i++){
+
+                System.out.println(Arrays.toString(n[i]));
+
+            }
+
+
+
+            for (int i = 0; i < tama; i++){
+
+                m[i] = subcorteza(n, i);
+
+
+            }
+
+
+
+
+
+
+            for (int i = 7; i < 11; i++){
+
+                //System.out.println(+subcorteza(n, i));
+
+
+            }
+
+            System.out.println(" *");
+            for (int j = 7; j < 11; j++){
+
+                System.out.println(+subcorteza(n, j));
+
+
+            }
+
+
+        }
+}
