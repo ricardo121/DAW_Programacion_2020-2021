881070088e9ca17bd47712362e8c1b86f276759b
ricardo121 <ricardoasimbaya@gmail.com>
2021-03-18 17:26:41 +0100

    Add files via upload

---
+++ Chimborazo_Asimbaya_U7_T1zip/EntregableU6U7/src/Alumno.java
@@ -0,0 +1,12 @@
+public class Alumno {
+
+    String nombreAlumno;
+    String apellidos;
+
+    public Unidad(String nombreAlumno){
+        this.nombreAlumno = nombreAlumno ;
+        this.apellidos = apellidos ;
+
+    }
+
+}
---
+++ Chimborazo_Asimbaya_U7_T1zip/EntregableU6U7/src/Unidad.java
@@ -0,0 +1,12 @@
+public class Unidad {
+
+    String nombreUnidad;
+
+    public Unidad(String nombreUnidad){
+        this.nombreUnidad = nombreUnidad ;
+
+    }
+
+
+
+}
